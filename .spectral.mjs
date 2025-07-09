afcffddf2de784d93ee0a22665efa1488ffc880f
ddfsdfd444fdddfd <neelam.chavan@smartbear.com>
2025-07-09 17:49:50 +0530

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-20280.stoplight-dev.com/api/v1/projects/cHJqOjE1NDg/spectral.js?branch_slug=main&token=40fb3e13-760f-46bc-bceb-8ccadcd351b2";
+export default { extends: ruleset };
